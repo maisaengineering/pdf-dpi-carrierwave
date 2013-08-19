0db70ea23bd43c268b5df224f98e9c42278db3bf
maisaengineering <contact@maisasolutions.com>
2013-08-19 10:43:55 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdfDpiCarrierwave::Application.config.secret_token = '7e1a7b600098f59a3bbad5e982424fcb18d08c109df30f95f4e141fed59be655ae3a2e63eaf2efe264c0f56ce381f88322d3e561baedd733868cb1b5bb30c2bb'
